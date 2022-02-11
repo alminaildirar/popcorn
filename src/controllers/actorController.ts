aa76e3c81e39546fce1a01d76eb723bf8643aeeb
alminaildirar <alminaildirar@gmail.com>
2022-02-11 23:45:51 +0300

    added error handler to create actor and film apis

--- src/controllers/actorController.ts
+++ src/controllers/actorController.ts
@@ -1,96 +1,100 @@
-import { RequestHandler} from "express";
+import { RequestHandler } from "express";
 import { User } from "../entity/User";
 import { Actor } from "../entity/Actor";
 import { ActorLikes } from "../entity/ActorLikes";
 import { ActorComments } from "../entity/ActorComments";
-import * as fs from 'fs'
-
-
+import * as fs from "fs";
 
 export const addActor: RequestHandler = async (req, res) => {
-    try {
+  try {
+
+    if (!req.userID) res.redirect("/login");
     
-      if (!req.userID) res.redirect("/login");
-      const currentUser = await User.findOne({ id: req.userID });
-  
-      const uploadDir = "public/uploads"
-      if(!fs.existsSync(uploadDir)){
-        fs.mkdirSync(uploadDir)
-      }
-  
-      const imageName  = req.files.image['name'];
-      const image = req.files.image['data']
-  
-      fs.writeFileSync(uploadDir + '/' + imageName,image);
-      const imageurl = '/uploads/' + imageName
-  
-      const { name, description } = req.body;
-      let visibility;
-      req.body.visibility ? (visibility = false) : (visibility = true);
-      const actor = await Actor.create({
-        name,
-        description,
-        visible: visibility,
-        image: imageurl,
-        user: currentUser,
-      });
-      await Actor.save(actor);
-      //YÖNLENDİRME DEĞİŞECEK!!!
-      res.redirect("/dash");
-    } catch (error) {}
+    if (!req.body.name || !req.body.description) {
+      const errors: string = "Actor name/Actor description is required.";
+      return res.render("add-actor", { errors });
+    }
+
+    
+    const currentUser = await User.findOne({ id: req.userID });
+
+    const uploadDir = "public/uploads";
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+
+    const imageName = req.files.image["name"];
+    const image = req.files.image["data"];
+
+    fs.writeFileSync(uploadDir + "/" + imageName, image);
+    const imageurl = "/uploads/" + imageName;
+
+    const { name, description } = req.body;
+    let visibility;
+    req.body.visibility ? (visibility = false) : (visibility = true);
+    const actor = await Actor.create({
+      name,
+      description,
+      visible: visibility,
+      image: imageurl,
+      user: currentUser,
+    });
+    await Actor.save(actor);
+    //YÖNLENDİRME DEĞİŞECEK!!!
+    res.redirect("/dash");
+  } catch (error) {}
 };
 
 export const getActor: RequestHandler = async (req, res) => {
-    const currentUser = await User.findOne({id: req.userID})
-    const actor = await Actor.createQueryBuilder("actor")
-      .leftJoinAndSelect("actor.user", "user")
-      .leftJoinAndSelect("actor.comments", "comments.content")
-      .leftJoinAndSelect("actor.likes", "likes")
-      .where("actor.id = :actorID", { actorID: req.params.id })
-      .getOne();
-  
-    if (!actor) {
-      return res.redirect("/dash");
-    }
-  
-    let liked = false;
-    for (let i = 0; i < actor.likes.length; i++) {
-      actor.likes[i].ownerID == req.userID ? (liked = true) : (liked = false);
-    }
-  
-    res.render("actor", { actor, liked, user: currentUser.username });
+  const currentUser = await User.findOne({ id: req.userID });
+  const actor = await Actor.createQueryBuilder("actor")
+    .leftJoinAndSelect("actor.user", "user")
+    .leftJoinAndSelect("actor.comments", "comments.content")
+    .leftJoinAndSelect("actor.likes", "likes")
+    .where("actor.id = :actorID", { actorID: req.params.id })
+    .getOne();
+
+  if (!actor) {
+    return res.redirect("/dash");
+  }
+
+  let liked = false;
+  for (let i = 0; i < actor.likes.length; i++) {
+    actor.likes[i].ownerID == req.userID ? (liked = true) : (liked = false);
+  }
+
+  res.render("actor", { actor, liked, user: currentUser.username });
 };
 
 export const getAllActors: RequestHandler = async (req, res) => {
+  const page = req.query.page || 1;
+  const totalActors = (await Actor.find({ visible: true })).length;
 
-    const page = req.query.page || 1;
-    const totalActors = (await Actor.find({ visible: true })).length;
-  
-    const actors = await Actor.createQueryBuilder("actor")
-      .leftJoinAndSelect("actor.user", "user")
-      .leftJoinAndSelect("actor.comments", "comments")
-      .leftJoinAndSelect("actor.likes", "likes")
-      .take(5)
-      .skip((Number(page) - 1) * 5)
-      .where("actor.visible = true")
-      .orderBy("actor.created", "DESC")
-      .getMany();
-  
-    const userlikes = await ActorLikes.createQueryBuilder("actorlikes")
-      .leftJoinAndSelect("actorlikes.actor", "actor")
-      .where("actorlikes.ownerID = :userID", { userID: req.userID })
-      .getMany();
-  
-    const useractorLikes = [];
-    for (let i = 0; i < userlikes.length; i++) {
-      useractorLikes.push(userlikes[i].actor.id);
-    }
-    res.render("actors", {
-      actors,
-      useractorLikes,
-      pages: Math.ceil(totalActors / 5),
-      current: page,
-    });
+  const actors = await Actor.createQueryBuilder("actor")
+    .leftJoinAndSelect("actor.user", "user")
+    .leftJoinAndSelect("actor.comments", "comments")
+    .leftJoinAndSelect("actor.likes", "likes")
+    .take(5)
+    .skip((Number(page) - 1) * 5)
+    .where("actor.visible = true")
+    .orderBy("actor.created", "DESC")
+    .getMany();
+
+  const userlikes = await ActorLikes.createQueryBuilder("actorlikes")
+    .leftJoinAndSelect("actorlikes.actor", "actor")
+    .where("actorlikes.ownerID = :userID", { userID: req.userID })
+    .getMany();
+
+  const useractorLikes = [];
+  for (let i = 0; i < userlikes.length; i++) {
+    useractorLikes.push(userlikes[i].actor.id);
+  }
+  res.render("actors", {
+    actors,
+    useractorLikes,
+    pages: Math.ceil(totalActors / 5),
+    current: page,
+  });
 };
 
 export const getMyActors: RequestHandler = async (req, res) => {
@@ -130,7 +134,7 @@ export const likeActor: RequestHandler = async (req, res) => {
     return res.redirect("/dash");
   } else if (req.params.src === "single") {
     return res.redirect(`/actor/${req.params.id}`);
-  }else if(req.params.src === "all"){
+  } else if (req.params.src === "all") {
     return res.redirect(`/actor/actors`);
   }
 
@@ -138,7 +142,6 @@ export const likeActor: RequestHandler = async (req, res) => {
 };
 
 export const relikeActor: RequestHandler = async (req, res) => {
-
   const userlikes = await ActorLikes.createQueryBuilder("actorlikes")
     .leftJoinAndSelect("actorlikes.actor", "actor")
     .where("actorlikes.ownerID = :userID", { userID: req.userID })
@@ -154,54 +157,50 @@ export const relikeActor: RequestHandler = async (req, res) => {
     return res.redirect("/dash");
   } else if (req.params.src === "single") {
     return res.redirect(`/actor/${req.params.id}`);
-  }else if(req.params.src === "all"){
+  } else if (req.params.src === "all") {
     return res.redirect(`/actor/actors`);
   }
 
   res.redirect("/actor/my-actors");
 };
 
-export const addActorComment:RequestHandler = async(req,res) => {
-    
-  const currentUser = await User.findOne({id: req.userID})
-  
-  const currentActor = await Actor.findOne({id: Number(req.params.id)})
-  const {content} = req.body
+export const addActorComment: RequestHandler = async (req, res) => {
+  const currentUser = await User.findOne({ id: req.userID });
+
+  const currentActor = await Actor.findOne({ id: Number(req.params.id) });
+  const { content } = req.body;
 
   const comment = ActorComments.create({
-      content,
-      author: currentUser.username,
-      actor: currentActor
-  })
+    content,
+    author: currentUser.username,
+    actor: currentActor,
+  });
 
-  await ActorComments.save(comment)
+  await ActorComments.save(comment);
 
-  res.redirect(`/actor/${req.params.id}`)
-
+  res.redirect(`/actor/${req.params.id}`);
 };
 
-export const deleteCommentActor:RequestHandler = async (req, res) => {
-  
-  const commentToBeDeleted = await ActorComments.findOne({ id: Number(req.params.id)})
+export const deleteCommentActor: RequestHandler = async (req, res) => {
+  const commentToBeDeleted = await ActorComments.findOne({
+    id: Number(req.params.id),
+  });
   await ActorComments.remove(commentToBeDeleted);
 
-  res.redirect(`/actor/${req.params.actorID}`)
-
+  res.redirect(`/actor/${req.params.actorID}`);
 };
 
 export const updateActor: RequestHandler = async (req, res) => {
-  
-  const uploadDir = "public/uploads"
-  if(!fs.existsSync(uploadDir)){
-    fs.mkdirSync(uploadDir)
+  const uploadDir = "public/uploads";
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
   }
 
-  const imageName  = req.files.image['name'];
-  const image = req.files.image['data']
+  const imageName = req.files.image["name"];
+  const image = req.files.image["data"];
 
-  fs.writeFileSync(uploadDir + '/' + imageName,image);
-  const imageurl = '/uploads/' + imageName
-
+  fs.writeFileSync(uploadDir + "/" + imageName, image);
+  const imageurl = "/uploads/" + imageName;
 
   const actorToBeCheck = await Actor.createQueryBuilder("actor")
     .leftJoinAndSelect("actor.user", "user")
@@ -240,6 +239,3 @@ export const deleteActor: RequestHandler = async (req, res) => {
 
   return res.redirect("/dash");
 };
-
-
-
--- src/controllers/filmController.ts
+++ src/controllers/filmController.ts
@@ -1,96 +1,99 @@
-import { RequestHandler} from "express";
+import { RequestHandler } from "express";
 import { Film } from "../entity/Film";
 import { FilmLikes } from "../entity/FilmLikes";
 import { User } from "../entity/User";
-import * as fs from 'fs'
+import * as fs from "fs";
 import { FilmComments } from "../entity/FilmComments";
-
+import { RepositoryNotFoundError } from "typeorm";
 
 export const addFilm: RequestHandler = async (req, res) => {
-    try {
+  try {
     
-      
-      if (!req.userID) res.redirect("/login");
-      const currentUser = await User.findOne({ id: req.userID });
-  
-      const uploadDir = "public/uploads"
-      if(!fs.existsSync(uploadDir)){
-        fs.mkdirSync(uploadDir)
-      }
-  
-      const imageName  = req.files.image['name'];
-      const image = req.files.image['data']
-  
-      fs.writeFileSync(uploadDir + '/' + imageName,image);
-      const imageurl = '/uploads/' + imageName
-  
-      const { name, description } = req.body;
-      let visibility;
-      req.body.visibility ? (visibility = false) : (visibility = true);
-      const film = await Film.create({
-        name,
-        description,
-        visible: visibility,
-        image: imageurl,
-        user: currentUser,
-      });
-      await Film.save(film);
-      //YÖNLENDİRME DEĞİŞECEK!!!
-      res.redirect("/dash");
-    } catch (error) {}
+    if (!req.userID) return res.redirect("/login");
+
+    if (!req.body.name || !req.body.description) {
+      const errors: string = "Film name/Film description is required.";
+      return res.render("add-film", { errors });
+    }
+    
+    const currentUser = await User.findOne({ id: req.userID });
+
+    const uploadDir = "public/uploads";
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+
+    const imageName = req.files.image["name"];
+    const image = req.files.image["data"];
+    fs.writeFileSync(uploadDir + "/" + imageName, image);
+    const imageurl = "/uploads/" + imageName;
+
+    const { name, description } = req.body;
+    let visibility: boolean;
+    req.body.visibility ? (visibility = false) : (visibility = true);
+    const film = await Film.create({
+      name,
+      description,
+      visible: visibility,
+      image: imageurl,
+      user: currentUser,
+    });
+    await Film.save(film);
+    //YÖNLENDİRME DEĞİŞECEK!!!
+    res.redirect("/dash");
+  } catch (error) {}
 };
 
 export const getFilm: RequestHandler = async (req, res) => {
-    const currentUser = await User.findOne({id: req.userID})
-    const film = await Film.createQueryBuilder("film")
-      .leftJoinAndSelect("film.user", "user")
-      .leftJoinAndSelect("film.comments", "comments.content")
-      .leftJoinAndSelect("film.likes", "likes")
-      .where("film.id = :filmID", { filmID: req.params.id })
-      .getOne();
-  
-    if (!film) {
-      return res.redirect("/dash");
-    }
-  
-    let liked = false;
-    for (let i = 0; i < film.likes.length; i++) {
-      film.likes[i].ownerID == req.userID ? (liked = true) : (liked = false);
-    }
-  
-    res.render("film", { film, liked, user: currentUser.username });
+  const currentUser = await User.findOne({ id: req.userID });
+  const film = await Film.createQueryBuilder("film")
+    .leftJoinAndSelect("film.user", "user")
+    .leftJoinAndSelect("film.comments", "comments.content")
+    .leftJoinAndSelect("film.likes", "likes")
+    .where("film.id = :filmID", { filmID: req.params.id })
+    .getOne();
+
+  if (!film) {
+    return res.redirect("/dash");
+  }
+
+  let liked = false;
+  for (let i = 0; i < film.likes.length; i++) {
+    film.likes[i].ownerID == req.userID ? (liked = true) : (liked = false);
+  }
+
+  res.render("film", { film, liked, user: currentUser.username });
 };
 
-  export const getAllFilms: RequestHandler = async (req, res) => {
+export const getAllFilms: RequestHandler = async (req, res) => {
+  const page = req.query.page || 1;
+  const totalFilms = (await Film.find({ visible: true })).length;
 
-    const page = req.query.page || 1;
-    const totalFilms = (await Film.find({ visible: true })).length;
-  
-    const films = await Film.createQueryBuilder("film")
-      .leftJoinAndSelect("film.user", "user")
-      .leftJoinAndSelect("film.comments", "comments")
-      .leftJoinAndSelect("film.likes", "likes")
-      .take(5)
-      .skip((Number(page) - 1) * 5)
-      .where("film.visible = true")
-      .orderBy("film.created", "DESC")
-      .getMany();
-  
-    const userlikes = await FilmLikes.createQueryBuilder("filmlikes")
-      .leftJoinAndSelect("filmlikes.film", "film")
-      .where("filmlikes.ownerID = :userID", { userID: req.userID })
-      .getMany();
-  
-    const userfilmLikes = [];
-    for (let i = 0; i < userlikes.length; i++) {
-      userfilmLikes.push(userlikes[i].film.id);
-    }
-    res.render("films", {
-      films,
-      userfilmLikes,
-      pages: Math.ceil(totalFilms / 5),
-      current: page,
-    });
+  const films = await Film.createQueryBuilder("film")
+    .leftJoinAndSelect("film.user", "user")
+    .leftJoinAndSelect("film.comments", "comments")
+    .leftJoinAndSelect("film.likes", "likes")
+    .take(5)
+    .skip((Number(page) - 1) * 5)
+    .where("film.visible = true")
+    .orderBy("film.created", "DESC")
+    .getMany();
+
+  const userlikes = await FilmLikes.createQueryBuilder("filmlikes")
+    .leftJoinAndSelect("filmlikes.film", "film")
+    .where("filmlikes.ownerID = :userID", { userID: req.userID })
+    .getMany();
+
+  const userfilmLikes = [];
+  for (let i = 0; i < userlikes.length; i++) {
+    userfilmLikes.push(userlikes[i].film.id);
+  }
+  res.render("films", {
+    films,
+    userfilmLikes,
+    pages: Math.ceil(totalFilms / 5),
+    current: page,
+  });
 };
 
 export const getMyFilms: RequestHandler = async (req, res) => {
@@ -130,7 +133,7 @@ export const likeFilm: RequestHandler = async (req, res) => {
     return res.redirect("/dash");
   } else if (req.params.src === "single") {
     return res.redirect(`/film/${req.params.id}`);
-  }else if(req.params.src === "all"){
+  } else if (req.params.src === "all") {
     return res.redirect(`/film/films`);
   }
 
@@ -138,7 +141,6 @@ export const likeFilm: RequestHandler = async (req, res) => {
 };
 
 export const relikeFilm: RequestHandler = async (req, res) => {
-
   const userlikes = await FilmLikes.createQueryBuilder("filmlikes")
     .leftJoinAndSelect("filmlikes.film", "film")
     .where("filmlikes.ownerID = :userID", { userID: req.userID })
@@ -154,54 +156,50 @@ export const relikeFilm: RequestHandler = async (req, res) => {
     return res.redirect("/dash");
   } else if (req.params.src === "single") {
     return res.redirect(`/film/${req.params.id}`);
-  }else if(req.params.src === "all"){
+  } else if (req.params.src === "all") {
     return res.redirect(`/film/films`);
   }
 
   res.redirect("/film/my-films");
 };
 
-export const addFilmComment:RequestHandler = async(req,res) => {
-    
-  const currentUser = await User.findOne({id: req.userID})
-  
-  const currentFilm = await Film.findOne({id: Number(req.params.id)})
-  const {content} = req.body
+export const addFilmComment: RequestHandler = async (req, res) => {
+  const currentUser = await User.findOne({ id: req.userID });
+
+  const currentFilm = await Film.findOne({ id: Number(req.params.id) });
+  const { content } = req.body;
 
   const comment = FilmComments.create({
-      content,
-      author: currentUser.username,
-      film: currentFilm
-  })
+    content,
+    author: currentUser.username,
+    film: currentFilm,
+  });
 
-  await FilmComments.save(comment)
+  await FilmComments.save(comment);
 
-  res.redirect(`/film/${req.params.id}`)
-
+  res.redirect(`/film/${req.params.id}`);
 };
 
-export const deleteCommentFilm:RequestHandler = async (req, res) => {
-  
-  const commentToBeDeleted = await FilmComments.findOne({ id: Number(req.params.id)})
+export const deleteCommentFilm: RequestHandler = async (req, res) => {
+  const commentToBeDeleted = await FilmComments.findOne({
+    id: Number(req.params.id),
+  });
   await FilmComments.remove(commentToBeDeleted);
 
-  res.redirect(`/film/${req.params.filmID}`)
-
+  res.redirect(`/film/${req.params.filmID}`);
 };
 
 export const updateFilm: RequestHandler = async (req, res) => {
-  
-  const uploadDir = "public/uploads"
-  if(!fs.existsSync(uploadDir)){
-    fs.mkdirSync(uploadDir)
+  const uploadDir = "public/uploads";
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
   }
 
-  const imageName  = req.files.image['name'];
-  const image = req.files.image['data']
+  const imageName = req.files.image["name"];
+  const image = req.files.image["data"];
 
-  fs.writeFileSync(uploadDir + '/' + imageName,image);
-  const imageurl = '/uploads/' + imageName
-
+  fs.writeFileSync(uploadDir + "/" + imageName, image);
+  const imageurl = "/uploads/" + imageName;
 
   const filmToBeCheck = await Film.createQueryBuilder("film")
     .leftJoinAndSelect("film.user", "user")
@@ -240,4 +238,3 @@ export const deleteFilm: RequestHandler = async (req, res) => {
 
   return res.redirect("/dash");
 };
-
